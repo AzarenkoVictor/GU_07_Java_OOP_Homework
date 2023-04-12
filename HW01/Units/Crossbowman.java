35dd169dc01f5836d0846f930e6ba60b840054a7
Victor <114249624+AzarenkoVictor@users.noreply.github.com>
2023-04-12 23:18:11 +0300

    Delete HW01 directory

--- HW01/Units/Crossbowman.java
+++
@@ -1,7 +0,0 @@
-package Units;
-
-public class Crossbowman extends Archer {
-     public Crossbowman(String name, int level, int maxHp, int baseDamage, int baseProtection, int arrows) {
-          super(name, level, maxHp, baseDamage, baseProtection, arrows);
-     }
-}
--- HW01/Units/Spearman.java
+++
@@ -1,7 +0,0 @@
-package Units;
-
-public class Spearman extends Countryman {
-     public Spearman(String name, int level, int maxHp, int baseDamage, int baseProtection) {
-          super(name, level, maxHp, baseDamage, baseProtection);
-     }
-}
